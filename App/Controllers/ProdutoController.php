8d2c5edf9d2d691e6d950ac45158bb4c5280bc53
Josano <josano@hotmail.com>
2019-05-21 21:00:06 -0300

    erro produtos

--- App/Controllers/ProdutoController.php
+++ App/Controllers/ProdutoController.php
@@ -1,25 +1,29 @@
 <?php
-
-namespace  App\Controllers;
-
+namespace App\Controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-
+use App\DAO\MySQL\GerenciadorDeLojas\ProdutosDAO;
 final class ProdutoController
 {
-    public function getProdutos(Request $request, Response $response, array $args) : Response
+    public function getProdutos(Request $request, Response $response, array $args): Response
+    {
+        $queryParams = $request->getQueryParams();
+        $produtosDAO = new ProdutosDAO();
+        $id = (int)$queryParams['loja_id'];
+        $produtos = $produtosDAO->getAllProdutos($id);
+        $response = $response->withJson($produtos);
+        return $response;
+    }
+    public function insertProduto(Request $request, Response $response, array $args): Response
+    {
+
+        return $response;
+    }
+    public function updateProduto(Request $request, Response $response, array $args): Response
     {
         return $response;
     }
-    public function InsertProdutos(Request $request, Response $response, array $args) : Response
-    {
-        return $response;
-    }
-    public function UpdateProdutos(Request $request, Response $response, array $args) : Response
-    {
-        return $response;
-    }
-    public function deleteProdutos(Request $request, Response $response, array $args) : Response
+    public function deleteProduto(Request $request, Response $response, array $args): Response
     {
         return $response;
     }
